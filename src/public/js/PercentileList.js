5959f913dd8c4d02731fc576f8c5de2cc5be2e18
Peerawit Tungwongsin <t.peerawit@gmail.com>
2017-03-27 15:16:27 +0700

    Fix browser compatible issue

--- src/public/js/PercentileList.js
+++ src/public/js/PercentileList.js
@@ -1,54 +1,50 @@
-class PercentileList {
-    constructor () {}
+function PercentileList() {}
 
-    static get ITEM_TEMPLATE() {
-        return _.template(
-            '<li class="list-group-item clickable" data-text="<%- label %> <%- percentile %>%">' +
-                '<span class="list-group-item-heading">' +
-                    '<%- label %>' +
-                    '<%= hasChild ? \'&nbsp;<span href="#" class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>\' : \'\' %>' +
-                '</span>' +
-                '<div class="progress">' +
-                    '<div class="progress-bar" role="progressbar" aria-valuenow="<%- percentile %>" aria-valuemin="0" aria-valuemax="100" style="width: <%- percentile %>%;">' +
-                        '<%- percentile %>%' +
-                    '</div>' +
-                '</div>' +
-            '</li>'
-        );
-    }
+PercentileList.ITEM_TEMPLATE = _.template(
+    '<li class="list-group-item clickable" data-text="<%- label %> <%- percentile %>%">' +
+        '<span class="list-group-item-heading">' +
+            '<%- label %>' +
+            '<%= hasChild ? \'&nbsp;<span href="#" class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>\' : \'\' %>' +
+        '</span>' +
+        '<div class="progress">' +
+            '<div class="progress-bar" role="progressbar" aria-valuenow="<%- percentile %>" aria-valuemin="0" aria-valuemax="100" style="width: <%- percentile %>%;">' +
+                '<%- percentile %>%' +
+            '</div>' +
+        '</div>' +
+    '</li>'
+);
 
-    static render(items) {
-        var $html = $('<ul class="list-group"></ul>');
-        if (!_.isEmpty(items)) {
-            items.forEach((item) => {
-                var $item = $(PercentileList.ITEM_TEMPLATE({
-                    label: item.name,
-                    hasChild: !_.isEmpty(item.children),
-                    percentile: (item.percentile * 100).toFixed(2)
-                }));
+PercentileList.render = function (items) {
+    var $html = $('<ul class="list-group"></ul>');
+    if (!_.isEmpty(items)) {
+        items.forEach(function (item) {
+            var $item = $(PercentileList.ITEM_TEMPLATE({
+                label: item.name,
+                hasChild: !_.isEmpty(item.children),
+                percentile: (item.percentile * 100).toFixed(2)
+            }));
 
-                $item.click((e) => {
-                    TextToSpeech.speak($item.data('text'));
+            $item.click(function (e) {
+                TextToSpeech.speak($item.data('text'));
+                e.stopPropagation();
+            });
+
+            // Render expandable child nodes
+            if (!_.isEmpty(item.children)) {
+                var nodeId = new Date().getTime();
+                var $title = $item.find('.list-group-item-heading');
+                $title.click(function (e) {
+                    $title.find('.glyphicon-chevron-down, .glyphicon-chevron-up')
+                            .toggleClass('glyphicon-chevron-up glyphicon-chevron-down');
+                    $('#' + nodeId).toggleClass('hidden');
                     e.stopPropagation();
                 });
-
-                // Render expandable child nodes
-                if (!_.isEmpty(item.children)) {
-                    var nodeId = new Date().getTime();
-                    var $title = $item.find('.list-group-item-heading');
-                    $title.click((e) => {
-                        $title.find('.glyphicon-chevron-down, .glyphicon-chevron-up')
-                                .toggleClass('glyphicon-chevron-up glyphicon-chevron-down');
-                        $('#' + nodeId).toggleClass('hidden');
-                        e.stopPropagation();
-                    });
-                    $item.append(this.render(item.children)
-                            .attr('id', nodeId)
-                            .addClass('hidden'));
-                }
-                $html.append($item);
-            });
-        }
-        return $html;
+                $item.append(PercentileList.render(item.children)
+                        .attr('id', nodeId)
+                        .addClass('hidden'));
+            }
+            $html.append($item);
+        });
     }
-}
+    return $html;
+};
--- src/public/js/TextToSpeech.js
+++ src/public/js/TextToSpeech.js
@@ -1,18 +1,19 @@
-class TextToSpeech {
-    static get speaker() {
-        var speaker = document.getElementById('speaker');
-        if (!speaker) {
-            speaker = document.createElement('audio');
-            document.body.appendChild(speaker);
-        }
-        return speaker;
-    }
+function TextToSpeech() {}
 
-    static speak(text) {
-        var speaker = TextToSpeech.speaker;
-        speaker.src = '/api/synthesize?' + $.param({ voice: 'en-US_MichaelVoice', text: text });
-        speaker.pause();
-        speaker.load();
-        speaker.oncanplaythrough = () => { speaker.play(); };
+TextToSpeech.getSpeaker = function () {
+    var speaker = document.getElementById('speaker');
+    if (!speaker) {
+        speaker = document.createElement('audio');
+        speaker.setAttribute('id', 'speaker');
+        document.body.appendChild(speaker);
     }
-}
+    return speaker;
+};
+
+TextToSpeech.speak = function (text) {
+    var speaker = TextToSpeech.getSpeaker();
+    speaker.src = '/api/synthesize?' + $.param({ voice: 'en-US_MichaelVoice', text: text });
+    speaker.pause();
+    speaker.load();
+    speaker.oncanplaythrough = function () { speaker.play(); };
+};
